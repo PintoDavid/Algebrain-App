094346134d4c5f67fda2e2238ab2aaf4974391bc
PintoDavid <est.david.pinto@unimilitar.edu.co>
2024-05-11 12:42:03 -0500

    correccion contenido javascript

--- scripts/contenido_3.1.js
+++ scripts/contenido_3.1.js
@@ -1,39 +1,39 @@
 const lecciones = [
   {
-    0: "Introducción al sistema de ecuaciones lineales.",
-    1: "Es una técnica fundamental en álgebra lineal para resolver sistemas de ecuaciones lineales. Su objetivo es transformar un sistema de ecuaciones lineales en otro equivalente pero más simple, hasta llegar a una forma escalonada o reducida que permita fácilmente encontrar la solución.",
+    0: "Introducción al sistema de ecuaciones lineales",
+    1: "bla bla bla sistemas de ecuaciones lineales. Su objetivo es transformar un sistema de ecuaciones lineales en otro equivalente pero más simple, hasta llegar a una forma escalonada o reducida que permita fácilmente encontrar la solución.",
+    2: "https://www.youtube.com/embed/p2AIFY1b9qk?si=m_v06liUj9F4eJIm",
+    3: {
+      0: 1,
+      1: 10,
+      2: 999,
+      3: 100,
+    },
+    4: 4,
+  },
+  {
+    0: "Introducción al sistema de ecuaciones lineales",
+    1: "bla 2 bla 2 bla 2 resolver sistemas de ecuaciones lineales. Su objetivo es transformar un sistema de ecuaciones lineales en otro equivalente pero más simple, hasta llegar a una forma escalonada o reducida que permita fácilmente encontrar la solución.",
     2: "./img/IMG1.png",
     3: {
       0: 1,
       1: 10,
       2: 999,
-      3: 100
+      3: 100,
     },
-    4: 4
+    4: 4,
   },
   {
-    0: "Introducción al sistema de ecuaciones lineales.",
+    0: "Introducción al sistema de ecuaciones lineales",
     1: "Es una técnica fundamental en álgebra lineal para resolver sistemas de ecuaciones lineales. Su objetivo es transformar un sistema de ecuaciones lineales en otro equivalente pero más simple, hasta llegar a una forma escalonada o reducida que permita fácilmente encontrar la solución.",
     2: "./img/IMG1.png",
     3: {
       0: 1,
       1: 10,
       2: 1100,
-      3: 100
+      3: 100,
     },
-    4: 2
-  }
-
-]
-const lessons = [
-  { type: "video", content: "Insertar video aquí" },
-  { type: "texto", content: "Insertar texto aquí" },
-  { type: "ejercicio", content: "Insertar ejercicio aquí" },
-  {
-    type: "respuestaMultiple",
-    question: "¿Cuál es la capital de Francia?",
-    options: ["París", "Madrid", "Londres", "Roma"],
-    answer: "París",
+    4: 2,
   },
 ];
 
@@ -43,87 +43,73 @@ let correctAnswerSelected = false;
 const lessonCounter = document.getElementById("lessonCounter");
 const nextButton = document.getElementById("nextButton");
 const confirmButton = document.getElementById("confirmButton");
-const titulo = document.getElementById("Titulo")
-const contenido = document.getElementById("Texto")
-const imgT = document.getElementById("imgT")
-const opt1 = document.getElementById("opt1")
-const opt2 = document.getElementById("opt2")
-const opt3 = document.getElementById("opt3")
-const opt4 = document.getElementById("opt4")
+const titulo = document.getElementById("Titulo");
+const contenido = document.getElementById("Texto");
+const imgT = document.getElementById("imgT");
+const video = document.getElementById("video");
+const opt1 = document.getElementById("opt1");
+const opt2 = document.getElementById("opt2");
+const opt3 = document.getElementById("opt3");
+const opt4 = document.getElementById("opt4");
 
 titulo.textContent = lecciones[0][0];
 function updateLessonContent(valor) {
-  /* const lesson = lessons[currentLesson];
   lessonCounter.textContent = `Lección ${currentLesson + 1} de ${
-    lessons.length
+    lecciones.length
   }`;
-  lessonContent.innerHTML = "";
-
-  if (lesson.type === "respuestaMultiple") {
-    const exerciseDiv = document.createElement("div");
-    exerciseDiv.classList.add("exercise");
-
-    const questionElement = document.createElement("h2");
-    questionElement.textContent = lesson.question;
-    exerciseDiv.appendChild(questionElement);
-
-    const optionsList = document.createElement("ul");
-    lesson.options.forEach((option) => {
-      const optionItem = document.createElement("li");
-      const optionInput = document.createElement("input");
-      optionInput.type = "radio";
-      optionInput.name = "respuesta";
-      optionInput.value = option;
-      optionItem.appendChild(optionInput);
-      optionItem.appendChild(document.createTextNode(option));
-      optionsList.appendChild(optionItem);
-    });
-    exerciseDiv.appendChild(optionsList);
-
-    lessonContent.appendChild(exerciseDiv);
-    nextButton.disabled = true;
-    confirmButton.style.display = "block";
+  contenido.textContent = lecciones[valor][1];
+  if (valor > 0) {
+    imgT.src = lecciones[valor][2];
+    //texto
+    opt1.textContent = lecciones[valor][3][0];
+    opt2.textContent = lecciones[valor][3][1];
+    opt3.textContent = lecciones[valor][3][2];
+    opt4.textContent = lecciones[valor][3][3];
   } else {
-    lessonContent.textContent = lesson.content;
-    nextButton.disabled = false;
-    confirmButton.style.display = "none";
-  } */
-  
-  lessonCounter.textContent = `Lección ${currentLesson+1} de ${lecciones.length}`;
-  contenido.textContent = lecciones[valor][1]
-  imgT.src = lecciones[valor][2]
-  //texto 
-  opt1.textContent = lecciones[valor][3][0]
-  opt2.textContent = lecciones[valor][3][1]
-  opt3.textContent = lecciones[valor][3][2]
-  opt4.textContent = lecciones[valor][3][3]
-
-  //botones
-  opt1.addEventListener('click', () => {
-    correctAnswerSelected=false;
-    if (lecciones[valor][4] !== 1) { alert('¡Hiciste clic en el botón!'); return; }
-    alert('Solucionado');
-    correctAnswerSelected=true;
-  })
-  opt2.addEventListener('click', () => {
-    correctAnswerSelected=false;
-    if (lecciones[valor][4] !== 2) { alert('¡Hiciste clic en el botón!'); return; }
-    alert('Solucionado');
-    correctAnswerSelected=true;
-  })
-  opt3.addEventListener('click', () => {
-    correctAnswerSelected=false;
-    if (lecciones[valor][4] !== 3) { alert('¡Hiciste clic en el botón!'); return; }
-    alert('Solucionado');
-    correctAnswerSelected=true;
-  })
-  opt4.addEventListener('click', () => {
-    correctAnswerSelected=false;
-    if (lecciones[valor][4] !== 4) { alert('¡Hiciste clic en el botón!'); return; }
-    alert('Solucionado');
-    correctAnswerSelected=true;
-  })
+    video.src = lecciones[valor][2];
+    opt1.style.display = "none";
+    opt2.style.display = "none";
+    opt3.style.display = "none";
+    opt4.style.display = "none";
+  }
+  if (valor == lecciones.length - 1) {
+    nextButton.textContent = "Finalizar";
+  }
 }
+//botones
+correctAnswerSelected = false;
+opt1.addEventListener("click", () => {
+  if (lecciones[currentLesson][4] !== 1) {
+    alert("¡Hiciste clic en el botón!");
+    return 0;
+  }
+  alert("Solucionado");
+  correctAnswerSelected = true;
+});
+opt2.addEventListener("click", () => {
+  if (lecciones[currentLesson][4] !== 2) {
+    alert("¡Hiciste clic en el botón!");
+    return 0;
+  }
+  alert("Solucionado");
+  correctAnswerSelected = true;
+});
+opt3.addEventListener("click", () => {
+  if (lecciones[currentLesson][4] !== 3) {
+    alert("¡Hiciste clic en el botón!");
+    return 0;
+  }
+  alert("Solucionado");
+  correctAnswerSelected = true;
+});
+opt4.addEventListener("click", () => {
+  if (lecciones[currentLesson][4] !== 4) {
+    alert("¡Hiciste clic en el botón!");
+    return 0;
+  }
+  alert("Solucionado");
+  correctAnswerSelected = true;
+});
 
 document.getElementById("backButton").addEventListener("click", () => {
   showBackMenuPopup();
@@ -134,7 +120,7 @@ document.getElementById("cancelBackButton").addEventListener("click", () => {
 });
 
 document.getElementById("confirmBackButton").addEventListener("click", () => {
-  window.location.href = "seleccion_temas.html";
+  window.location.href = "Temas.html";
   hideBackMenuPopup();
 });
 
@@ -153,14 +139,45 @@ document.getElementById("prevButton").addEventListener("click", () => {
     currentLesson--;
     updateLessonContent(currentLesson);
   }
+  if (currentLesson == 0) {
+    correctAnswerSelected = false;
+    video.style.display = "block";
+    imgT.style.display = "none";
+    opt1.style.display = "none";
+    opt2.style.display = "none";
+    opt3.style.display = "none";
+    opt4.style.display = "none";
+    updateLessonContent(currentLesson);
+    return 0;
+  }
 });
 
 document.getElementById("nextButton").addEventListener("click", () => {
-  if (currentLesson < lecciones.length - 1 && correctAnswerSelected===true) {
+  if (currentLesson == 0) {
     currentLesson++;
+    correctAnswerSelected = false;
+    video.style.display = "none";
+    imgT.style.display = "block";
+    opt1.style.display = "block";
+    opt2.style.display = "block";
+    opt3.style.display = "block";
+    opt4.style.display = "block";
     updateLessonContent(currentLesson);
+    return 0;
   }
-  if(correctAnswerSelected!==true) alert('Por favor seleciona la respuesta correcta')
+  if (currentLesson < lecciones.length - 1 && correctAnswerSelected === true) {
+    currentLesson++;
+    correctAnswerSelected = false;
+    updateLessonContent(currentLesson);
+    return 0;
+  }
+  if (correctAnswerSelected !== true)
+    alert("Por favor seleciona la respuesta correcta");
+  if (currentLesson == lecciones.length - 1 && correctAnswerSelected === true) {
+    correctAnswerSelected = false;
+    window.location.href = "Temas.html";
+    currentLesson = 0;
+  }
 });
 
 document.getElementById("confirmButton").addEventListener("click", () => {
@@ -194,4 +211,4 @@ document.getElementById("closePopupButton").addEventListener("click", () => {
   popup.style.display = "none";
 });
 
-updateLessonContent(currentLesson)
+updateLessonContent(currentLesson);
